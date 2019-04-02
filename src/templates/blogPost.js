faa7b47b4e7bbbd93b45425c03abf1a325b2f986
Hong Jeesoo <jeesoo.hong@gmail.com>
2019-04-02 21:03:09 +0900

    정리 안된 자료 삭제, favicon 추가

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
 import './blogPost.scss';
+
 import Layout from '../components/Layout';
+import favicon from '../favicon-96x96.png';
 
 const Template = ({ data, pageContext }) => {
   const { next, prev } = pageContext;
@@ -10,26 +14,28 @@ const Template = ({ data, pageContext }) => {
   const title = markdownRemark.frontmatter.title;
   const htmlBody = markdownRemark.html;
   return (
-    <Layout>
-      <h1>{title}</h1>
-      <div className="post" dangerouslySetInnerHTML={{ __html: htmlBody }} />
-      <div className="postButton">
-        <div>
-          {next && (
-            <Link to={next.frontmatter.path}>
-              Next: {`${next.frontmatter.title}`}
-            </Link>
-          )}
+    <>
+      <Layout>
+        <h1>{title}</h1>
+        <div className="post" dangerouslySetInnerHTML={{ __html: htmlBody }} />
+        <div className="postButton">
+          <div>
+            {next && (
+              <Link to={next.frontmatter.path}>
+                Next: {`${next.frontmatter.title}`}
+              </Link>
+            )}
+          </div>
+          <div>
+            {prev && (
+              <Link to={prev.frontmatter.path}>
+                Prev: {`${prev.frontmatter.title}`}
+              </Link>
+            )}
+          </div>
         </div>
-        <div>
-          {prev && (
-            <Link to={prev.frontmatter.path}>
-              Prev: {`${prev.frontmatter.title}`}
-            </Link>
-          )}
-        </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
