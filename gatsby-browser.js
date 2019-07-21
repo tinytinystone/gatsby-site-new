d332d7e136bd3ac1438d9a9d540d422e5274b6f4
Jeesoo Hong <jeesoo.hong@gmail.com>
2019-07-21 21:39:16 +0900

    delete mathjax

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,10 +6,3 @@
 
 // You can delete this file if you're not using it
 require('prismjs/themes/prism.css');
-
-exports.onRouteUpdate = ({ location }) => {
-  console.log('new pathname', location.pathname);
-  if (window.MathJax !== undefined) {
-    MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
-  }
-};
